e0a1ade4d76d96ad1f482a180cccc1d43d6dbc72
9qUmV4 <9qumv4@gmail.com>
2023-07-19 17:23:07 +0200

    Made sort_by_percentage much faster

--- src/wordleBot/functions.py
+++ src/wordleBot/functions.py
@@ -16,39 +16,14 @@ def sort_by_percentage(wl: pd.Series) -> pd.Series:
     if isinstance(wl, str):
         return pd.Series(data=[wl,], index=[1, ])
     # Split up string into single chars
-    dt = wl.str.split("", expand=True)
-    dt: pd.DataFrame = dt.drop(columns=[0, 6]) # Drop start and end of string (Whitespace chars)
-    dt.columns = range(dt.columns.size)     # TODO reset_index(drop=True)?
-
-    # Set up dataframe to count appearances of characters based on their position 
-    dt_c = pd.DataFrame(data=np.nan, index=list(string.ascii_lowercase), columns=range(5))
-
-    # Fill in the count dataframe
-    for c in string.ascii_lowercase:
-        dt_tmp: pd.DataFrame = (dt == c).replace({False: 0, True: 1})
-        dt_c.loc[c, :] = dt_tmp.sum(axis="index", skipna=True)
-
-    # Calculate percentages
-    dt_cn = (dt_c / len(wl))
-
-    # Calculate the percentage for every word
-    wl_per = dt
-    for col in range(len(dt.columns)):
-        con_table = dt_cn.loc[:, col].to_dict()
-        wl_per.loc[:,col].replace(con_table, inplace=True)
-
-    wl_per['total'] = wl_per.sum(axis='columns') / 5
-
-    # Add words into dataframe
-    wl_per['word'] = wl
-
-    # Sort based on percentage
-    wl_per_sort = wl_per.sort_values('total', axis='index', ascending=False)
-
-    wl_per_sort = wl_per_sort.loc[:, ['total', 'word']]     # Create a new Series with total:word and sort that
-    wl_per_sort = wl_per_sort.set_index('total').squeeze()
-
-    return wl_per_sort
+    dt = wl.str.split("", expand=True).iloc[:,1:-1]
+    
+    
+    dt_c = dt.apply(pd.value_counts, normalize=True, sort=False)
+    dt_percentage = dt.replace(dt_c).sum("columns") / 5
+    
+    df_sorted = pd.concat({"word": wl, "percentage": dt_percentage}, axis=1).sort_values("percentage", ascending=False)
+    return df_sorted["word"]
 
 
 def read(path: Union[str, pathlib.Path], length: int) -> pd.Series:
